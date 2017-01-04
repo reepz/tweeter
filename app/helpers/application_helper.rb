1fcc2d969bd6a0f041f364446342ffcb001bc11b
reepz <a.wojtowicz@outlook.com>
2017-01-04 02:03:05 +0100

    working static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(title = "")
+  base_title = "Twi77er"
+    if title.empty?
+      base_title
+    else
+      title + " | " + base_title
+    end
+  end
+    
 end
