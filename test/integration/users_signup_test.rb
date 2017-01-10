1ed9be009bfa57b071c5a7add328da8b1369077a
reepz <a.wojtowicz@outlook.com>
2017-01-10 20:03:41 +0100

    SSL and Puma for production

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                         email: "foo@invalid",
+                                         password: "emo",
+                                         password_confirmation: "ticon" } }
+      end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Zibi Stono",
+                                         email: "kancelaria@premiera.jp",
+                                         password: "czubashek",
+                                         password_confirmation: "czubashek" }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
+end
