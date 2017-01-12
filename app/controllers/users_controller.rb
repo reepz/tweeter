cbd4450e68894145d8698238bfc7f19a06cd6c17
reepz <a.wojtowicz@outlook.com>
2017-01-12 23:24:05 +0100

    basic login implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Twi77er App!"
       redirect_to @user
       # alternative way
