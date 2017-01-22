3dec3aef4cdf9f1576c7b40f89f4c975f0f4bb02
reepz <a.wojtowicz@outlook.com>
2017-01-22 13:54:49 +0100

    adjust delete post button

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -26,7 +26,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_match content, response.body
     # Delete post
-    assert_select 'a', text: 'delete'
+    assert_select 'a', text: 'Remove'
     first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference "Micropost.count", -1 do
       delete micropost_path(first_micropost)
